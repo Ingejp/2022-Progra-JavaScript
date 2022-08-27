77d25610189f5f81d7fad5e0e8aa020b9b42fac5
Juan Pablo Israel Escobar Castro <ingejp@outlook.com>
2022-08-27 10:33:02 -0600

    Update script.js
    Se agrega el codigo visto en segunda clase de JavaScript

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -61,6 +61,87 @@ function manipularElementosDelDom(){
     correo.value='';
 }
 
+function crearElementosEnEjecusion() {
+	let divCreadoEnEjecusion = document.createElement('div');	
+	let otroElemento = document.createElement('h4');
+	let botones = document.createElement('button');
+	otroElemento.append('El titulo deseado');
+	divCreadoEnEjecusion.append("texto a agregar");
+    botones.setAttribute("value", "Click Aqui");
+	let container= document.getElementById("principal");
+	container.append(otroElemento);
+	container.append(divCreadoEnEjecusion);
+	container.append(botones);
+ 	//console.log(divCreadoEnEjecusion); 		
+}
 
+function eliminarElementosEnEjecusion(){
+	let container= document.getElementById("principal");
+	container.remove();
+}
+
+function desestructurarArrays(){
+	let miArray=['uno', 'dos','tres'];
+
+	let [a, b, c]=miArray;
+
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+function manejoDeExcepciones(){
+	try{
+		algoQueNoExiste();
+	}catch(error){
+		console.log(error);
+		console.error('Errores del try catch');
+		console.error(error);
+	}
+	
+}
+
+function enviarFormulario(){
+	let request = new XMLHttpRequest();
+request.open('POST', '/php/email-sender.php', true);
+request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+request.onreadystatechange = function() {
+  if (request.readyState == XMLHttpRequest.DONE) {
+    // Peticion terminada.
+    if (request.status == 200) {
+      // Todo salio bien
+      console.log(request.response);
+    } else {
+      console.log("Error en el envío " + request.response);
+    }
+  }
+}
+
+var formData = new FormData(document.getElementById('contact-form'));
+request.send(formData);
+}
+
+function enviarFormularioOpcion2(){
+	const XHR = new XMLHttpRequest();
+	  var formData = new FormData(document.getElementById('contact-form'));
+
+	  // Define what happens on successful data submission
+	  XHR.addEventListener('load', (event) => {
+	    alert('Yeah! Data sent and response loaded.');
+	  });
+
+	  // Define what happens in case of error
+	  XHR.addEventListener('error', (event) => {
+	    alert('Oops! Something went wrong.');
+	  });
+
+	  // Set up our request
+	  XHR.open('POST', 'https://example.com/cors.php');
+
+	  // Send our FormData object; HTTP headers are set automatically
+	  XHR.send(formData);
+	
+}
 
 
